4d945c130f7e726c08166ee942ca732c26d27c5f
timothyhudsonemail <60154902+timothyhudsonemail@users.noreply.github.com>
2020-10-27 16:11:43 -0500

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+
+app.get('/', (req, res) =>{
+    res.send('Helloww')
+})
+
+module.exports = app
